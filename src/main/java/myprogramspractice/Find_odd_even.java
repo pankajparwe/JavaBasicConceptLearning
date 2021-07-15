00f70e24dae2bd9644d5f8db46cd1e33ba2ccc3f
Pankaj_parwe <pankaj.parwe@gmail.com>
2021-07-16 00:14:00 +0530

    fd

---
+++ src/main/java/myprogramspractice/Find_odd_even.java
@@ -0,0 +1,19 @@
+package myprogramspractice;
+
+public class Find_odd_even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a=14;
+	if(a%2==0) 
+	
+		System.out.println("number is even" );
+	else
+		
+     	System.out.println("number is odd");
+	
+	
+
+}
+}
